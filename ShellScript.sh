991e39ff3b2b7b8fb083e66a15e32ec1517c8e31
Dhavasree <42274406+Dhavasree@users.noreply.github.com>
2019-03-19 12:27:43 +0530

    clone the project from git and rename the file and push it back to same repo
    clone the project from git and rename the file and push it back to same repo

--- ShellScript.sh
+++ ShellScript.sh
@@ -1,26 +1,29 @@
-
-
-echo "Enter username"
-read username
+dir=$(pwd)
+echo $dir
+username=$(whoami)
+echo $username
+#echo "Enter git username"
+#read username
 echo "Enter the repository name"
 read repo
 #start chrome https://github.com/"$username"/"$repo"
 #url="https://"$username":"$password"@github.com/"$username"/"$repo""
 url="https://github.com/"$username"/"$repo""
+echo $url
 echo "Enter the foldername for copying the project from github to D directory"
 read foldername
 mkdir $foldername
-path="D:/${foldername}"
+path="$dir/${foldername}"
+echo $path
 git clone $url $path
 echo "Copied the file from github to D directory"
-echo "Enter the Project Name"
-read oldproject
-echo "Enter the new project name"
+ oldproject="project-template"
+echo "Enter the Destination project name"
 read newproject
 space=" "
-newproject=${newproject//-/${space}}
-newproject="$(echo "${newproject}" | perl -pe 's/\S+/\u$&/g')"
-newproject=${newproject//${space}}
+newproject1=${newproject//-/${space}}
+newproject1="$(echo "${newproject1}" | perl -pe 's/\S+/\u$&/g')"
+newproject1=${newproject1//${space}}
 cd $path
 mkdir ./$newproject
 
@@ -36,10 +39,7 @@ do
 done 
 echo "File has been transferred from $oldproject to $newproject"
 
-f1="D:${foldername}/"
-f2="${newproject}"
-f3="\config\pipeline\views"
-filepath=$f1$f2$f3
+filepath="$path/${newproject}/config/pipeline/views"
 cd ${filepath}
 for entry in "$filepath"/*
 do
@@ -47,16 +47,16 @@ do
 done 
 fname=`basename $entry`
 echo $fname
-mv "$fname" "${fname//project_name_camel_case/"${newproject}"}"
+mv "$fname" "${fname//project_name_camel_case/"${newproject1}"}"
 for entry in "$filepath"/*
 do
   echo $entry
-  sed -i 's/<project_name_camelcase>/'${newproject}'/g' $entry
+  sed -i 's/<project_name_camelcase>/'${newproject1}'/g' $entry
 done 
 
 
-f4="${newproject}\config"
-filepath=$f1$f4
+
+filepath="$path/${newproject}/config"
 cd ${filepath}
 ls
 for entry in "$filepath"/*
@@ -74,13 +74,13 @@ do
   if [ $fname = "pipeline.groovy" ]
   then
   sed -i 's/<project_name>/'${newproject}'/g' $entry
-  sed -i 's/<project_name_camelcase>/'${newproject}'/g' $entry
-  variable=${newproject//[^[:alnum:]]/}
+  sed -i 's/<project_name_camelcase>/'${newproject1}'/g' $entry
+  variable=${newproject1//[^[:alnum:]]/}
   sed -i 's/<project_name_wo_special_characters>/'${variable}'/g' $entry
   fi
 done
 
-filepath="D:/${foldername}/${newproject}"
+filepath="$path/${newproject}"
 echo $filepath
 cd ${filepath}
 for entry in "$filepath"/*
@@ -95,7 +95,13 @@ do
   then
   sed -i 's/'$oldproject'/'${newproject}'/g' $entry
   sed -i 's/gfb-data/'${newproject}'/g' $entry
-  sed -i 's/projectTemplate/'${newproject}'/g' $entry
+  sed -i 's/projectTemplate/'${newproject1}'/g' $entry
   fi
 done 
 
+git rm -r --cached .
+git add "$filepath"
+git commit -m "newcommit"
+git remote set-url origin https://github.com/"$username"/"$repo"
+git push -u origin master
+echo "File has been saved in git"
